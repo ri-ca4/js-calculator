680bc8872b40578decd63018e75e939af7eb7c76
ri-ca4 <riley.de12@gmail.com>
2022-06-16 08:48:08 -0400

    changed btn to input type btn, created var for input, started css

---
+++ js/calculator.js
@@ -0,0 +1,25 @@
+/*
+    author: ri-ca4
+    date: 6/2022
+    title: calculator
+*/
+
+//buttons
+var multiply = document.getElementById('multiply');
+var divide   = document.getElementById('divide');
+var add      = document.getElementById('add');
+var subtract = document.getElementById('subtract');
+var back     = document.getElementById('back');
+var clear    = document.getElementById('clear');
+var enter    = document.getElementById('enter');
+var dec      = document.getElementById('dec');
+var one      = document.getElementById('one');
+var two      = document.getElementById('two');
+var three    = document.getElementById('three');
+var four     = document.getElementById('four');
+var five     = document.getElementById('five');
+var six      = document.getElementById('six');
+var seven    = document.getElementById('seven');
+var eight    = document.getElementById('eight');
+var nine     = document.getElementById('nine');
+
