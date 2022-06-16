b039fb83dcefdc370c470ee81d5077dc76062e0c
ri-ca4 <riley.de12@gmail.com>
2022-06-16 09:16:28 -0400

    deleted unused var, added onclick events, tested

--- js/calculator.js
+++ js/calculator.js
@@ -4,22 +4,18 @@
     title: calculator
 */
 
-//buttons
-var multiply = document.getElementById('multiply');
-var divide   = document.getElementById('divide');
-var add      = document.getElementById('add');
-var subtract = document.getElementById('subtract');
-var back     = document.getElementById('back');
-var clear    = document.getElementById('clear');
-var enter    = document.getElementById('enter');
-var dec      = document.getElementById('dec');
-var one      = document.getElementById('one');
-var two      = document.getElementById('two');
-var three    = document.getElementById('three');
-var four     = document.getElementById('four');
-var five     = document.getElementById('five');
-var six      = document.getElementById('six');
-var seven    = document.getElementById('seven');
-var eight    = document.getElementById('eight');
-var nine     = document.getElementById('nine');
+var display   = document.getElementById('display');
+var userInput = '';
 
+function inputNum(e){
+    userInput += e.value;
+    display.innerHTML = userInput;
+}
+
+function inputOp(e){
+    if(userInput == ''){
+        return
+    }
+    userInput += e.value;
+    display.innerHTML = userInput;
+}
