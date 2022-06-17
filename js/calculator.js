2f7a7070ee26ea8b99e5ae976851e314ecbc027e
ri-ca4 <riley.de12@gmail.com>
2022-06-17 14:45:30 -0400

    finished onclick functions, need to fix calculate errors

--- js/calculator.js
+++ js/calculator.js
@@ -4,33 +4,105 @@
     title: calculator
 */
 
-var display   = document.getElementById('display');
+//define variables
+const display   = document.getElementById('display');
 var userInput = '';
+var operators = ['*', '/', '+', '-'];
 
-
-function inputNum(e){
+function inputNum(e){//add new number to string
     userInput += e.value;
     display.innerHTML = userInput;
 }
 
-function inputOp(e){
-    if(userInput == ''){
-        return
-    }
-    userInput += e.value;
-    display.innerHTML = userInput;
+function inputOp(e){//when operator button is pressed
+    var operator = e.value;
+    var nums = [];
+    if(userInput == ''){//if there is no input do nothing
+        return;
+    }else if(userInput.indexOf('*') == -1
+           &&userInput.indexOf('/') == -1
+           &&userInput.indexOf('+') == -1
+           &&userInput.indexOf('-') == -1){//if there is no operator
+            console.log('there no operator')
+            userInput += ' ' + e.value + ' ';//add operator to string
+            display.innerHTML = userInput;
+        }else{//if there is an operator
+            var arr  = userInput.split(' '); //make string into array
+            for(i=0; i<arr.length; i++){ //convert string to numbers
+                if(!operators.includes(arr[i])){//skip over operator
+                //convert strings to numbers
+                    nums.push(parseFloat(arr[i]));
+                    console.log(nums)
+                }
+            }
+            result = calculate(operator, nums);//calculate result
+            userInput = result.toString();//result to string
+            userInput += ' ' + e.value + ' ';//add new operator to string
+            display.innerHTML = userInput;
+        }
 }
 
-function clearAll(){
+function clearAll(){//delete all user input
     userInput = '';
     display.innerHTML = userInput;
 }
 
-function backSpace(){
-    userInput = userInput.slice(0, -1);
-    display.innerHTML = userInput;
+function backSpace(){//removing last number or operator
+    var lastChar = userInput.slice(-1);
+    if(lastChar == ' '){//if last character is a space remove operator and spaces around
+        userInput = userInput.slice(0, -3);
+        display.innerHTML = userInput;
+    }else{//remove last number
+        userInput = userInput.slice(0, -1);
+        display.innerHTML = userInput;
+    }
 }
 
-function calculate(){
+function equals(){//when equals is pressed
+    if(userInput == '' ){//if there is no input do nothing
+        return
+    }else{//check if entry is long enough and get numbers and operator
+        var arr  = userInput.split(' ');//create array
+        if(arr.length == 3){
+            var nums = [];
+            var operator;
+            for(i=0; i<arr.length; i++){ //convert string to numbers and operator
+                var arr  = userInput.split(' '); //make string into array
+                if(operators.includes(arr[i])){//assign operator value
+                    operator = arr[i];
+                }else{//convert string to number
+                    nums.push(parseFloat(arr[i]))
+                }
+            }
+            result = calculate(operator, nums);//calculate result
+            userInput = result.toString();//number to string
+            display.innerHTML = userInput;
+            console.log(userInput)
+        }else{
+            return
+        }
+    }
+}
 
+function calculate(operator, nums){//do the math
+    if(operator == '*'){//multiply
+        userInput = nums[0] * nums[1];
+        display.innerHTML = userInput;
+        return userInput;
+    }
+    if(operator == '/'){//divide
+        userInput = nums[0] / nums[1];
+        display.innerHTML = userInput;
+        return userInput;
+    }
+    if(operator == '+'){//add
+        userInput = nums[0] + nums[1];
+        display.innerHTML = userInput;
+        return userInput;
+    }
+    if(operator == '-'){//subtract
+        userInput = nums[0] - nums[1];
+        display.innerHTML = userInput;
+        return userInput;
+    }
 }
